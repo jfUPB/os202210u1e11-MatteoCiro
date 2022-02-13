e1399fe3832a1721bd7fc1090c28c63f634fc2f6
MatteoCiro <mateo.agudeloc@upb.edu.co>
2022-02-13 10:31:31 -0500

    Avance 2

--- problem1/eventList.c
+++ problem1/eventList.c
@@ -6,24 +6,27 @@
 EventList *CreateEventList(void)
 {
     EventList *eventList = NULL;
-
     return eventList;
 }
-
 void DestroyEventList(EventList *this)
 {
-
-
     free(this);
 }
-
 Event *SearchEvent(EventList *this, char *name)
 {
-
-
-    return NULL;
+    Event *a = this->head;
+    if(this->isEmpty == 0){
+        return NULL;
+    }
+    else{
+        while (a!= NULL){
+            if(*(a->eventName + 2) == *(name + 2) && *(a->eventName + 3) == *(name + 3)){
+                return a;
+            }
+            a = a->next;
+        }
+    }
 }
-
 void AddEvent(EventList *this, Event *event)
 {
 

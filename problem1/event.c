bdfd362a6b472be6e425758b867035b9cb80fef8
MatteoCiro <mateo.agudeloc@upb.edu.co>
2022-02-13 10:22:23 -0500

    Avance 1

--- problem1/event.c
+++ problem1/event.c
@@ -4,10 +4,12 @@
 
 Event *CreateEvent(char *name)
 {
-    Event *event = NULL;
+    Event *event = malloc(sizeof(Event));
+    name[15]=0;
+    sscanf(name, "%s",event->eventName);
+    event->next=NULL;
     return event;
 }
-
 void DestroyEvent(Event *this)
 {
     free(this);

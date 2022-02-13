921ffd8f8b0abc05fa26148c2a355511876791cb
MatteoCiro <mateo.agudeloc@upb.edu.co>
2022-02-13 11:04:51 -0500

    Avance 3 y Correción

--- problem1/eventList.c
+++ problem1/eventList.c
@@ -5,7 +5,7 @@
 
 EventList *CreateEventList(void)
 {
-    EventList *eventList = NULL;
+    EventList *eventList = malloc(sizeof(eventList));
     return eventList;
 }
 void DestroyEventList(EventList *this)
@@ -15,34 +15,66 @@ void DestroyEventList(EventList *this)
 Event *SearchEvent(EventList *this, char *name)
 {
     Event *a = this->head;
-    if(this->isEmpty == 0){
+    if (this->isEmpty == 0){
         return NULL;
     }
     else{
         while (a!= NULL){
-            if(*(a->eventName + 2) == *(name + 2) && *(a->eventName + 3) == *(name + 3)){
+            if (*(a->eventName + 2) == *(name + 2) && *(a->eventName + 3) == *(name + 3)){
                 return a;
             }
             a = a->next;
         }
     }
+    DestroyEvent(a);
+    return NULL;
 }
 void AddEvent(EventList *this, Event *event)
 {
-
+    Event *a = this->head;
+    if (this->isEmpty!= 0){
+        while (a!= NULL){
+            if (*(a->eventName + 2) == *(event->eventName + 2) && *(a->eventName + 3) == *(event->eventName + 3)){
+                return;
+            }
+            a = a->next;
+        }
+        this->last->next=event;
+        this->last=event;
+    }
+    else{
+        this->head = event;
+        this->last = event;
+        this->isEmpty = 1;
+    }
+    DestroyEvent(a);
 }
-
 void RemoveEvent(EventList *this, char *name)
 {
-
+    Event *a = this->head;
+    Event *t = SearchEvent(this, name);
+    if (t == NULL)
+    return;
+    if (this->isEmpty == 0){
+        return;
+    }
+    else{
+        while (a!= NULL){
+            if (*(this->head->eventName + 2) == *(name + 2)){
+                this->head = this->head->next;
+                break;
+            }
+        }        
+    }
+    
 }
 void ListEvents(EventList *this)
 {
     Event *a = this->head;
-    if(this->isEmpty == 0)
+    if (this->isEmpty == 0)
     printf("empty\n");
     else{
-        while(a!= NULL){
+        while (a!= NULL){
             printf("%s\n", a->eventName);
             a = a->next;
         }
